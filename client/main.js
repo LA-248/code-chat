4c2c60fdcac93b633618ea88396873bab356b697
LA-248 <luca728@proton.me>
2023-07-04 20:26:46 +0200

    Feat - Add border to the selected programming language button

--- client/main.js
+++ client/main.js
@@ -1,14 +1,17 @@
-  import {
+import {
   selectLanguage,
+  displayQuestion,
   displayLoadingMessage,
   removeLoadingMessage,
   selection,
 } from './modules/user-interface.js';
 
 const answerBox = document.querySelector('.answer-text-box');
-const languageSelection = document.querySelector('.language-selection-cards');
+const languageSelection = document.querySelector('.language-selection-buttons');
+const questionInput = document.getElementById('question-input');
 const form = document.getElementById('form');
 
+// Make an API request to OpenAI's chat completion endpoint and display the message response as an answer
 async function getCompletion(message, language) {
   const url = 'https://api.openai.com/v1/chat/completions';
   const apiKey = '';
@@ -47,25 +50,48 @@ async function getCompletion(message, language) {
   }
 }
 
-function displayQuestion() {
-  const questionTextBox = document.querySelector('.question-text-box');
-  const input = document.getElementById('question-input').value;
-  questionTextBox.textContent = input;
-}
-
+// Function that displays the answer returned by the OpenAI API in the appropriate text box
 function displayAnswer(event) {
+  // Prevent the page from automatically refreshing on form submission
   event.preventDefault();
   const input = document.getElementById('question-input').value;
   getCompletion(input, selection);
 }
 
+// Sets the border for the card of the selected programming language
+function setLanguageButtonBorder(event) {
+  const clickedButton = event.target;
+  const allLanguageButtons = document.querySelectorAll('.language-button');
+
+  // Remove the 'active' class from all buttons when clicking on one
+  if (clickedButton.classList.contains('language-button')) {
+    allLanguageButtons.forEach(button => {
+      button.classList.remove('active');
+    });
+    // Add the 'active' class only to the button that was clicked
+    clickedButton.classList.add('active');
+  }
+}
+
 languageSelection.addEventListener('click', (event) => {
   selectLanguage(event);
+  setLanguageButtonBorder(event);
+  questionInput.disabled = false;
+  questionInput.style.backgroundColor = 'white';
 });
 
+// Once the user submits their question, display the answer returned and the question asked
 form.addEventListener('submit', (event) => {
   displayAnswer(event);
   displayQuestion();
-  const questionInput = document.getElementById('question-input');
   questionInput.value = '';
+  answerBox.textContent = '';
 });
+
+window.onload = () => {
+  questionInput.disabled = true;
+  if (questionInput.disabled === true) {
+    questionInput.style.backgroundColor = '#f8f8f8';
+    questionInput.placeholder = 'Please select a programming language';
+  }
+};
