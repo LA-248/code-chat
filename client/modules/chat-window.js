7864bf3a902914bdad5fe01c117411213515d7b2
Luca <luca728@proton.me>
2023-07-27 13:21:05 +0200

    Fix dependency cycle

--- client/modules/chat-window.js
+++ client/modules/chat-window.js
@@ -17,28 +17,4 @@ function displayAnswer(event) {
   getCompletion(input, selection);
 }
 
-// Create and display a loading message which is shown whilst the response to the question is being retrieved
-function displayLoadingMessage() {
-  const form = document.getElementById('form');
-  const loadingMessage = document.createElement('p');
-
-  loadingMessage.className = 'loadingMessage';
-  loadingMessage.textContent =
-    'Please wait while the request is being processed...';
-  form.appendChild(loadingMessage);
-
-  return loadingMessage;
-}
-
-// Remove the loading message once the answer has been retrieved
-function removeLoadingMessage(loadingMessage) {
-  const form = document.getElementById('form');
-  form.removeChild(loadingMessage);
-}
-
-export {
-  displayQuestion,
-  displayAnswer,
-  displayLoadingMessage,
-  removeLoadingMessage
-};
+export { displayQuestion, displayAnswer };
--- client/modules/openai-chat-api.js
+++ client/modules/openai-chat-api.js
@@ -1,5 +1,5 @@
 import fetchAPIKey from './api-key-fetcher.js';
-import { displayLoadingMessage, removeLoadingMessage } from './chat-window.js';
+import { displayLoadingMessage, removeLoadingMessage } from './loading-message.js';
 import { createRecentQuestion, saveQuestionToHistory } from './chat-history.js';
 
 // Make an API request to OpenAI's chat completion endpoint and display the message response as an answer
