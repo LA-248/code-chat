3c177631316ac724c96d071162fa9a4be0d2cf51
Luca <luca728@proton.me>
2023-08-01 16:08:00 +0200

    Feat - Replace language selection buttons with dropdown menu

--- client/modules/language-selection.js
+++ client/modules/language-selection.js
@@ -3,39 +3,26 @@ const questionInput = document.getElementById('question-input');
 
 // Set the programming language selected by the user
 function setLanguageSelection(event) {
-  if (event.target.className === 'language-button') {
-    selection = event.target.textContent;
-    questionInput.placeholder = `Ask any question about using ${selection}`;
-    console.log(selection);
-  }
-}
-
-// Sets the border for the button of the selected programming language
-function setLanguageButtonBorder(event) {
-  const clickedButton = event.target;
-  const allLanguageButtons = document.querySelectorAll('.language-button');
-
-  // Remove the 'active' class from all buttons when clicking on one
-  if (clickedButton.classList.contains('language-button')) {
-    allLanguageButtons.forEach((button) => {
-      button.classList.remove('active');
-    });
-    // Add the 'active' class only to the button that was clicked
-    clickedButton.classList.add('active');
-  }
-}
-
-// Set up the event listener for the language selection buttons - which calls the above functions when triggered
-function setSelectedLanguage() {
-  const languageSelection = document.querySelector(
-    '.language-selection-buttons'
-  );
-
-  languageSelection.addEventListener('click', (event) => {
-    setLanguageSelection(event);
-    setLanguageButtonBorder(event);
+  selection = event.target.value;
+  if (selection === 'Select') {
+    questionInput.disabled = true;
+    questionInput.style.backgroundColor = '#f8f8f8';
+    questionInput.placeholder = 'Please select a programming language';
+  } else {
     questionInput.disabled = false;
     questionInput.style.backgroundColor = 'white';
+    questionInput.placeholder = `Ask any question about using ${selection}`;
+  }
+  console.log(selection);
+}
+
+// Set up the event listener for the language selection dropdown menu -
+// which calls the 'setLanguageSelection' function when the user changes language in the dropdown menu
+function setSelectedLanguage() {
+  const dropdownMenu = document.getElementById('dropdown-menu');
+
+  dropdownMenu.addEventListener('change', (event) => {
+    setLanguageSelection(event);
   });
 }
 
