0deceb263beddf5e84e3d5f84157f3c56e53fb44
Luca <luca728@proton.me>
2023-08-05 16:16:38 +0200

    Fix cors middleware

--- server/api/index.js
+++ server/api/index.js
@@ -3,11 +3,12 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors(), {
+
+app.use(cors({
   origin: ['https://code-chat-frontend.vercel.app'],
   methods: ['POST', 'GET'],
   credentials: true,
-});
+}));
 
 app.get('/api/key', (req, res) => {
   const apiKey = process.env.API_KEY;
