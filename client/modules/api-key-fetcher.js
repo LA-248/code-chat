367fbf841fde8f61e6cbaded89e51d89a72c3fa8
Luca <luca728@proton.me>
2023-08-05 16:08:43 +0200

    Add the 'credentials' option in fetch calls

--- client/modules/api-key-fetcher.js
+++ client/modules/api-key-fetcher.js
@@ -2,6 +2,7 @@ export default async function fetchAPIKey() {
   try {
     const response = await fetch('https://code-chat-backend.vercel.app/api/key', {
       method: 'GET',
+      credentials: 'include',
       mode: 'cors',
       headers: {
         'Content-Type': 'application/json',
--- client/modules/openai-chat-api.js
+++ client/modules/openai-chat-api.js
@@ -14,6 +14,7 @@ export default async function getCompletion(message, language) {
   try {
     const response = await fetch(url, {
       method: 'POST',
+      credentials: 'include',
       mode: 'cors',
       headers: {
         'Content-Type': 'application/json',
