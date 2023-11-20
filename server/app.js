8decdb9d9398cbf10b547e0bc0d4bbeac6851972
LA-248 <luca728@proton.me>
2023-11-20 21:45:32 +0100

    Enable CORS using Node.js Serverless Functions deployed on Vercel

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -7,12 +6,22 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(cors());
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+}
 
-app.get('/api/key', (req, res) => {
+app.get('/api/key', allowCors(async (req, res) => {
   const apiKey = process.env.API_KEY;
   res.json({ apiKey });
-});
+}));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
