41999ed09ede8d986d2f7c60aa6d4eba2c67f99f
LA-248 <luca728@proton.me>
2023-11-20 23:59:39 +0100

    Set 'Access-Control-Allow-Origin' to a specific URL

--- server/api/get-completion.js
+++ server/api/get-completion.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', 'https://code-chat-site.vercel.app')
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
