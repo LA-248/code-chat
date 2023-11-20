4ec016a3418f98c9c601187d1793dcb60b6660e0
LA-248 <luca728@proton.me>
2023-11-20 18:54:39 +0100

    Modify CORS setup

--- server/api/index.js
+++ server/api/index.js
@@ -4,14 +4,24 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: ['https://code-chat-frontend.vercel.app'],
-  methods: ['POST', 'GET'],
-}));
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://code-chat-frontend.vercel.app'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.options('*', (req, res) => {
+  res.sendStatus(200);
+});
 
 app.get('/api/key', (req, res) => {
   const apiKey = process.env.API_KEY;
-  res.setHeader('Content-Type', 'application/json');
   res.json({ apiKey });
 });
 
